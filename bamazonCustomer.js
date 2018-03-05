0a26988e867c3716a5327f610660963e1a122992
Chris Hutchinson <chrisbond19@me.com>
2018-03-04 19:47:17 -0600

    Fixed connecton port and removed password

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -5,8 +5,14 @@ var inquirer = require('inquirer');
 //Connection method to bamazon database
 var connection = mysql.createConnection({
   host: "localhost",
-  port: 3000,
+  port: 3306,
   user: "root",
-  password: "123456",
+  password: "",
   database: "bamazon"
-})
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  //Start function after connection is made
+  start();
+});
